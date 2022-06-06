f1e2934fe148dcaed7e7768c3ccb74759bd8f8e5
PriCamEsteban <esteban1251994@hotmail.com>
2022-06-05 20:56:09 -0500

    nn3

---
+++ pagina web/src/index.js
@@ -0,0 +1,28 @@
+console.log("server works");
+const express = require ("express");
+const app = express();
+const path = require('path');
+//setting 
+app.set('port',3000);
+//middlewares
+
+
+//routes
+app.get('/', (req, res)=>{
+    //res.send('hola mundo');
+   // res.sendFile(path.join(__dirname,'/views/Site1/Inicio.html'));
+    res.sendFile(path.join(__dirname,'/views/Site1/proyecto.html'));
+    console.log(path.join(__dirname ,'views/paginaInicio.html'));
+});
+//static files
+
+
+//listening the server
+
+
+
+
+
+app.listen(app.get('port'),() => {
+    console.log('server on port',app.get('port'))
+});
