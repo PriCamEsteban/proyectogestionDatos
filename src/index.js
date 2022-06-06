81ce44add1a5ea5349430ec894576afc5f92a486
PriCamEsteban <esteban1251994@hotmail.com>
2022-06-05 22:52:16 -0500

    asdasd

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ app.set('port',PORT);
 
 //rute
 app.get('/',(req,res)=>{
-   //res.send('hola mundo');
-    res.sendFile(path.join(__dirname ,'htmlProyect/inicio.html'));
+   res.send('hola mundo');
+   // res.sendFile(path.join(__dirname ,'htmlProyect/inicio.html'));
     //res.render('inicio');
 });
 
 //listening the server
 app.listen(app.get('port'),()=>{
-    console.log(__dirname)
+    //console.log(__dirname)
 });
