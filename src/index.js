6e2b46966dfe4137ab53c6b191ab1496a4282bdf
PriCamEsteban <esteban1251994@hotmail.com>
2022-06-05 22:51:27 -0500

    asdasdaddd

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app= express();
-const parh =require('path');
+const path =require('path');
 //settings
-const PORT= process.env.PORT || 3000
-app.set('port',PORT)
+const PORT= process.env.PORT || 3000;
+app.set('port',PORT);
 
 //rute
 app.get('/',(req,res)=>{
-    res.send('hola mundo');
-    //res.sendFile(path.join(__dirname,'htmlProyect/inicio.html'));
+   //res.send('hola mundo');
+    res.sendFile(path.join(__dirname ,'htmlProyect/inicio.html'));
+    //res.render('inicio');
 });
 
 //listening the server
 app.listen(app.get('port'),()=>{
-    console.log("hola",app.get('port'))
+    console.log(__dirname)
 });
