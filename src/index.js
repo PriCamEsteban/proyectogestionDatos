273ec5361940133e5a4aa367575629abe264ef38
PriCamEsteban <esteban1251994@hotmail.com>
2022-06-05 22:10:01 -0500

    nndfs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app= express();
 //settings
-//const PORT= process.env.PORT
-app-set('port',3000)
+const PORT= process.env.PORT ||3000
+app.set('port',PORT)
 
 //rute
 app.get('/',(req,res)=>{
